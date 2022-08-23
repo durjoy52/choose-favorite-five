8e14cd2b2e3336590941e70db5e81399bab6d1f9
durjoy sarkar <durjoywebdeveloper777@gmail.com>
2022-08-23 17:50:54 +0600

    player listed solve

--- app.js
+++ app.js
@@ -1,3 +1,18 @@
+const players = []
 function addToCart(element){
-    console.log('player')
+    const player = element.parentNode.children[0].innerText
+    players.push(player)
+    const li =document.createElement('li')
+    const playerList =document.getElementById('player-list')
+    for(let player of players){
+    li.innerText = player
+    console.log(players)
+}
+if(players.length <= 5){
+    playerList.appendChild(li)
+}else{
+    alert('You cannot select more than 5 players')
+}
+const cardButton = element.parentNode.children[2]
+cardButton.setAttribute("disabled","disabled")
 }
